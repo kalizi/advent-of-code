f31cfba55994d161f3c0a9a88b01ee980c91357d
Kalizi <me@kalizi.dev>
2023-12-02 18:32:22 +0100

    Completed challenge 2 day 1

---
+++ 01/02.go
@@ -0,0 +1,87 @@
+/*
+ * Hello, I'm trying to learn Go with Advent of Code 2023.
+ */
+
+package main
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+    "os"
+)
+
+func readInput() string {
+    str, err := os.ReadFile("input.txt")
+    if err != nil {
+        fmt.Println("Error reading file")
+        panic(err)
+    }
+    return string(str)
+}
+
+func indexLookup(position int, line string, strlen int) int {
+    lookup := [9] string {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    }
+
+    for l := 0; l < 9; l++ {
+        if strings.HasPrefix(line[position:], lookup[l]) {
+            return l + 1
+        }
+    }
+
+    return -1
+}
+
+func main() {
+    str := readInput()
+
+    sum := 0
+	lines := strings.Split(str, "\n")
+
+	for _, line := range lines {
+        lineLen := len(line)
+        if lineLen > 0 {
+            var num, decimal int
+
+            i := 0
+            lookup := indexLookup(i, line, lineLen)
+            for (line[i] <= '0' || line[i] > '9') && (lookup == -1) {
+                i++
+                lookup = indexLookup(i, line, lineLen)
+            }
+            if lookup != -1 {
+                decimal = lookup
+            } else {
+                decimal, _ = strconv.Atoi(string(line[i]))
+            }
+
+            i = lineLen - 1
+            lookup = indexLookup(i, line, lineLen)
+            for (line[i] <= '0' || line[i] > '9') && (lookup == -1) {
+                i--
+                lookup = indexLookup(i, line, lineLen)
+            }
+            if lookup != -1 {
+                num = lookup
+            } else {
+                num, _ = strconv.Atoi(string(line[i]))
+            }
+
+            sum += decimal * 10 + num
+        }
+	}
+
+	fmt.Println(sum)
+}
+
+
